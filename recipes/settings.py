d50a3bb9494500234db3f8624f8d70da729de699
Aleksandr Pronskiy <pronskiy04@gmail.com>
2024-06-15 13:09:23 +0300

    Environment changes

--- recipes/settings.py
+++ recipes/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(os.environ.get("DEBUG", default=0))
 
 ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
 
